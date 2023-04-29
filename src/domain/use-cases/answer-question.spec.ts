87d570e2b94a2db1d9b8f96c705d6956cdf53e55
Raphael Berger <raphaelmb@gmail.com>
2023-04-29 10:38:10 -0300

    answer repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,9 +1,17 @@
 import { expect, test } from "vitest";
 import AnswerQuestionUseCase from "./answer-question";
+import AnswersRepository from "../repositories/answers-repository";
+import Answer from "../entities/answer";
 
-test("create an answer", () => {
-  const answerQuestion = new AnswerQuestionUseCase();
-  const answer = answerQuestion.execute({
+const fakeAnswersRepository: AnswersRepository = {
+  async create(answer: Answer): Promise<void> {
+    return;
+  },
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+  const answer = await answerQuestion.execute({
     instructorId: "1",
     questionId: "1",
     content: "new answer",
