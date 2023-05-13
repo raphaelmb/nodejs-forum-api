9813042770a972e5437351abcd4d55d547605465
Raphael Berger <raphaelmb@gmail.com>
2023-05-13 13:48:49 -0300

    add list question comment usecase

---
+++ src/domain/forum/application/use-cases/fetch-queston-comments.ts
@@ -0,0 +1,31 @@
+import QuestionComment from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionCommentsUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionCommentsUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export default class FetchQuestionCommentsUseCase {
+  constructor(
+    readonly questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionCommentsUseCaseRequest): Promise<FetchQuestionCommentsUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import QuestionComment from '@/domain/forum/enterprise/entities/question-comment'
 
@@ -12,6 +13,17 @@ export class InMemoryQuestionCommentsRepository
       return null
     }
     return questionComment
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
